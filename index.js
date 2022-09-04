53334ed655f67a6e59314bc78d824aaa25fe5fcd
Roman <mobile333t@gmail.com>
2022-09-04 20:11:13 +0300

    1

---
+++ index.js
@@ -0,0 +1,70 @@
+class MyArray {
+  constructor() {
+      this.length = 0;
+      for(let i=0;i<arguments.length;i++){
+       this.push(arguments[i]);
+
+      }
+      // TODO: реалізувати додавання елементів масиву при створенні екземпляру
+  }
+
+  static isMyArray(obj) {
+      return obj instanceof MyArray; 
+  }
+
+  push(...rest) {
+      for (let i = 0; i < rest.length; i++) {
+          this[this.length] = rest[i];
+          this.length++;
+      }
+  }
+
+  pop () {
+      let lastItem = this[this.length - 1];
+      delete this[this.length - 1];
+      this.length--;
+      return lastItem;
+  }
+
+  forEach (fn) {
+      for (let i = 0; i < this.length; i++) {
+          fn(this[i]);
+      }
+  }
+
+  map(fn) {
+      const newArr = new MyArray();
+      for (let i = 0; i < this.length; i++) {
+          newArr.push(fn(this[i], i, this));
+      }
+      return newArr;
+  }
+ 
+  [Symbol.iterator]() {
+      let i = 0;
+      return {
+          next: () => ({
+                  done: i > this.length - 1,
+                  value: this[i++]
+              })
+      }
+  }
+}
+
+const myarr = new MyArray();
+myarr.push(1, 2, 3, 4);
+
+
+/* 
+ДЗ: написати реалізацію методу flat() для MyArray()
+new MyArray(42,2,3, 1, new MyArray(3, 2, 1, 2, new MyArray(4, 2, 1, 2)));
+*/
+
+
+
+const myarr1 = new MyArray(42,2,3, 1, new MyArray(3, 2, 1, 2, new MyArray(4, 2, 1, 2)));
+const myarr3 = Array.from(myarr1).flat(2);
+console.log(myarr3);
+
+
+
